05bad3bf2ce4060bca4c14f7303333fd19d8143b
Fenyves96 <fenyves96@hotmail.com>
2020-04-19 17:51:15 +0200

    App func->class+flex

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: 'fasz',
+      customStyle: {
+        color: 'green'
+      }
+    }
+  }
+
+  onClickHandler = () => {
+    this.setState({
+      text: 'nemis',
+      customStyle: {
+        color: 'red'
+      }
+    })
+  }
+
+  render() {
+    return (
+
+      <View style={styles.container} >
+        <View style={styles.half} />
+        <View style={styles.half2} />
+        {/* <Button onPress={this.onClickHandler} title="asd" /> */}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row'
   },
+  half: {
+    flex: 1,
+    backgroundColor: 'red'
+  },
+  half2: {
+    flex: 1,
+    backgroundColor: 'green'
+  }
 });
+
+
